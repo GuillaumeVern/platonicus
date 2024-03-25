6f37610fc27147955d57dd145a4221d0be18cf71
ebourgeo <devebourgeo@gmail.com>
2024-03-25 12:07:42 +0100

    :fire: footer removed 2

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { BodyComponent } from '../body/body.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeaderComponent, BodyComponent, FooterComponent],
+  imports: [HeaderComponent, BodyComponent],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {
-
-}
+export class MainPageComponent {}
