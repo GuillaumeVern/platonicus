96ec0a40490c5bc61c8e752ca9a8520d9a0101d3
GuillaumeVern <guillaume.vern@outlook.com>
2024-07-10 16:04:22 +0200

    :sparkles: init leaderboard, settings and profile components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,17 @@ import { LoginComponent } from './components/body/login/login.component';
 import { ErrorComponent } from './components/body/error/error.component';
 import { RegisterComponent } from './components/body/register/register.component';
 import { authGuard } from './auth.guard';
+import { ProfileComponent } from './components/body/profile/profile.component';
+import { LeaderboardComponent } from './components/body/leaderboard/leaderboard.component';
+import { SettingsComponent } from './components/body/settings/settings.component';
 
 export const routes: Routes = [
-    { path: '', component: MainPageComponent, pathMatch: 'full', canActivate: [authGuard] },
+    { path: 'home', component: MainPageComponent, pathMatch: 'full', canActivate: [authGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
+    { path: 'leaderboard', component: LeaderboardComponent, canActivate: [authGuard] },
+    { path: 'settings', component: SettingsComponent, canActivate: [authGuard] },
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: '**', component: ErrorComponent },
 ];
